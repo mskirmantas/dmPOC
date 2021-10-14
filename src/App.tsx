f7ba09d73c8afe975f5543a7c08652b6f1253008
Mantas Skirmantas <mantas.skirmantas@yahoo.com>
2021-10-14 16:20:38 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,273 @@
 import React from "react";
+
+// import { useReactToPrint } from "react-to-print";
+
+// import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+// import Modal from "@mui/material/Modal";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   const currentURL = window.location.href;
   const urlStringParams = new URL(currentURL).searchParams;
   const params = Object.fromEntries(urlStringParams.entries());
 
   const ewmURL = params["LINK"];
 
-  console.log(params);
+  const [showModal, setShowModal] = React.useState<boolean>(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  // const componentRef = React.useRef();
+  // const handlePrint = useReactToPrint({
+  //   content: () => componentRef.current,
+  // });
+
+  const handlePrint = () => {
+    window.print();
+  };
 
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      <button>
-        <a href={ewmURL}>Cancel</a>
-      </button>
+      <div
+        className="header"
+        style={{ height: "100px", backgroundColor: "grey" }}
+      >
+        Header
+      </div>
 
-      <ul className="param-preview">
-        <h4>List of passed parameters via query string:</h4>
-        {Object.keys(params).map((keyName, i) => (
-          <li key={i}>
-            {keyName}: <span>{params[keyName]}</span>
-          </li>
-        ))}
-      </ul>
+      <div
+        className="content"
+        style={{ minHeight: "calc(100vh - 200px)", margin: 0 }}
+      >
+        <h1>Hello world!</h1>
+
+        <Button variant="contained">
+          <a href={ewmURL}>Cancel</a>
+        </Button>
+
+        <ul className="param-preview">
+          <h4>List of passed parameters via query string:</h4>
+          {Object.keys(params).map((keyName, i) => (
+            <li key={i}>
+              {keyName}: <span>{params[keyName]}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <footer
+        className="footer"
+        style={{ height: "100px", backgroundColor: "grey" }}
+      >
+        {/* <Modal open={showModal}>
+          <Box
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+
+              maxWidth: "1280px",
+              height: "100%",
+              // border: "2px solid #000",
+              backgroundColor: "white",
+              textAlign: "center",
+              padding: "40px",
+              overflowY: "scroll",
+            }}
+          >
+            <h1>This is modal</h1>
+            <Button variant="contained" onClick={toggleModal}>
+              Close
+            </Button>
+
+            <ul>
+              {" "}
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>{" "}
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.{" "}
+              </li>
+            </ul>
+            <Button
+              variant="contained"
+              onClick={handlePrint}
+              style={{ backgroundColor: "orange" }}
+            >
+              PRINT
+            </Button>
+          </Box>
+        </Modal> */}
+        {showModal && <Modal onPrint={handlePrint} onClose={toggleModal} />}
+
+        <Button variant="contained" onClick={toggleModal}>
+          Open modal
+        </Button>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
+
+interface ModalProps {
+  onClose: () => void;
+  onPrint: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ children, onClose, onPrint }) => {
+  return (
+    <div className="Modal">
+      <Button variant="contained" onClick={onClose}>
+        Close
+      </Button>
+      <div>{children}</div>
+      <ul style={{ padding: "50px" }}>
+        {" "}
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>{" "}
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.{" "}
+        </li>
+      </ul>
+      <Button
+        variant="contained"
+        onClick={onPrint}
+        style={{ backgroundColor: "green" }}
+      >
+        PRINT
+      </Button>
+    </div>
+  );
+};
+// export default Modal;
